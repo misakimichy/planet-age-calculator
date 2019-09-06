43ad49f4e7aa1d68cfaa0f63743f1bd632673ac2
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-06 11:00:36 -0700

    Add toFixed and return it as a number and test it.

--- spec/planet-age-calc-spec.js
+++ spec/planet-age-calc-spec.js
@@ -3,6 +3,9 @@ import { CurrentStatus } from '../src/main';
 const h1 = new CurrentStatus(10, "Japan", false, false);
 describe('currentStatus', function() {
   it('should return age', function() {
-    expect(h1.returnAge()).toEqual(10);
+    expect(h1.returnAge()[0]).toEqual(41.67);
+    expect(h1.returnAge()[1]).toEqual(16.13);
+    expect(h1.returnAge()[2]).toEqual(5.32);
+    expect(h1.returnAge()[3]).toEqual(0.84);
   });
 });
--- src/main.js
+++ src/main.js
@@ -10,7 +10,12 @@ export class CurrentStatus {
   }
 
   returnAge () {
-    return this.age;
+    const mercuryAge = parseFloat((this.age / 0.24).toFixed(2));
+    const venusAge = parseFloat((this.age / 0.62).toFixed(2));
+    const marsAge = parseFloat((this.age / 1.88).toFixed(2));
+    const jupiterAge = parseFloat((this.age / 11.86).toFixed(2));
+
+    return [mercuryAge, venusAge, marsAge, jupiterAge];
   }
 
 }
@@ -18,5 +23,5 @@ export class CurrentStatus {
 // Frontend logic
 $(document).ready(function(){
   const h1 = new CurrentStatus(10, "Japan", false, false);
-  return h1;
+  return h1.returnAge()[1];
 });
