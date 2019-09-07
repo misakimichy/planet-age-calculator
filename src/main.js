c1a8ad91646ce2169a9926172eafa366a2240a37
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-06 17:58:53 -0700

    Call backend logic in user interface.

--- src/main.js
+++ src/main.js
@@ -45,7 +45,6 @@ export class User {
         userLifeExpectancy = 82.2;
         break;
       default :
-        alert("Please choose country from the list.");
         throw new Error("Please choose country from the list.");
     }
     return userLifeExpectancy;
@@ -104,11 +103,20 @@ export class Planet {
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    const user = new User(10, "Japan");
-    const planet = new Planet("Mercury");
-  
-    $(".result").show();
-    return planet.calculateLeftToLive(user);
-  })
+    const inputAge = parseInt($("input#age").val());
+    const inputCountry = $("select#country").val();
+    const inputPlanet = $("select#planet").val();
 
+    const user = new User(inputAge, inputCountry);
+    const planet = new Planet(inputPlanet);
+
+    try {
+      $(".result").show();
+      $(".planet").text(planet.name);
+      $(".age").text(planet.calculateToPlanetAge(user));
+      $(".years-left").text(planet.calculateLeftToLive(user));
+    } catch (error) {
+      alert(error.message);
+    }
+  });
 });
