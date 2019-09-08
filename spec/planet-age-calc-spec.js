56929f1982bea1813571c90a28f8ac7126474f85
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-08 16:26:05 -0700

    Pass planet name to test object.

--- spec/planet-age-calc-spec.js
+++ spec/planet-age-calc-spec.js
@@ -10,19 +10,19 @@ describe('User', function() {
     expect(function(){user.calculateLifeExpectancy();}).toThrow(new Error("Please choose country from the list."));
   });
   it('should calculate years to live', function() {
-    const user1 = new User(10, countries.Japan, planets.Mercury);
+    const user1 = new User(10, countries.Japan, "Mercury");
     const result1 = user1.calculateAges();
     expect(result1.planetAge).toEqual(41.67);
     expect(result1.yearsToLive).toEqual(307.08);
-    const user2 = new User(10, countries.Japan, planets.Venus);
+    const user2 = new User(10, countries.Japan, "Venus");
     const result2 = user2.calculateAges();
     expect(result2.planetAge).toEqual(16.13);
     expect(result2.yearsToLive).toEqual(118.87);
-    const user3 = new User(10, countries.Japan, planets.Mars);
+    const user3 = new User(10, countries.Japan, "Mars");
     const result3 = user3.calculateAges();
     expect(result3.planetAge).toEqual(5.32);
     expect(result3.yearsToLive).toEqual(39.2);
-    const user4 = new User(10, countries.Japan, planets.Jupiter);
+    const user4 = new User(10, countries.Japan, "Jupiter");
     const result4 = user4.calculateAges();
     expect(result4.planetAge).toEqual(0.84);
     expect(result4.yearsToLive).toEqual(6.21);
